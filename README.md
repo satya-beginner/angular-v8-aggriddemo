998387a1ec78dd3ae9a1ff5b4b522e4115142155
satya-beginner <satya.beginner@gmail.com>
2021-03-22 10:26:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-v8-aggriddemo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-v8-aggriddemo)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
