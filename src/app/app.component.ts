e36cee2dd001b4b739c72217290ea98602767352
satya-beginner <satya.beginner@gmail.com>
2021-03-22 11:26:07 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { GridOptions } from "ag-grid";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'my-first-app';  
-columnDefs = [
-        { field: 'make' },
-        { field: 'model' },
-        { field: 'price'}
+  private gridOptions: GridOptions;
+
+  constructor() {
+    this.gridOptions = <GridOptions>{
+      enableSorting: true,
+      enableFilter: true
+    };
+
+    this.gridOptions.columnDefs = [
+      {
+        headerName: "make",
+        field: "make",
+        width: 100
+      },
+      {
+        headerName: "model",
+        field: "model",
+        width: 100
+      },
+      {
+        headerName: "price",
+        field: "price",
+        width: 100
+      }
     ];
 
-    rowData = [
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    this.gridOptions.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
     ];
-
+  }
 }
